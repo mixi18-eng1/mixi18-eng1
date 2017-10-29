474dd9690f4f6269463ee74b80fde1eb1f82b54a
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2017-10-30 03:30:39 +0900

    Fix config for permission

--- .circleci/git-empty-commit.sh
+++ .circleci/git-empty-commit.sh
@@ -3,5 +3,5 @@
 BRANCH_NAME="$1"
 git checkout -b "$BRANCH_NAME" origin/"$BRANCH_NAME"
 git commit --allow-empty -m "Empty!! [ci skip]"
-git push
+git push origin "$BRANCH_NAME"
 git checkout circleci
